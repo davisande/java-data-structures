d24b6bffb90ef8297e8342679ea3e7f58be17e96
Davi Sande <davisande@gmail.com>
2022-11-14 23:59:08 -0300

    Rename list package

--- src/list/EmployeeLinkedList.java
+++ src/list/EmployeeLinkedList.java
@@ -1,4 +1,4 @@
-package List;
+package list;
 
 public class EmployeeLinkedList {
     private EmployeeNode head;
--- src/list/EmployeeNode.java
+++ src/list/EmployeeNode.java
@@ -1,4 +1,4 @@
-package List;
+package list;
 
 public class EmployeeNode {
     private Employee employee;
